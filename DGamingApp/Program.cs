1d8e99c575cdc69d0649eefddf03043f31c61f33
lucas-perata <contacto.perata@gmail.com>
2023-03-19 22:30:44 -0300

    API skeleton including: migration, udpate db, interface, repository and controller

--- DGamingApp/Program.cs
+++ DGamingApp/Program.cs
@@ -1,11 +1,23 @@
+using DGamingApp.Data;
+using DGamingApp.Interfaces;
+using DGamingApp.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Repos
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
---
+++ DGamingApp/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using DGamingApp.Data;
+using DGamingApp.Entities;
+using DGamingApp.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DGamingApp.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<AppUser> GetUserById(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
