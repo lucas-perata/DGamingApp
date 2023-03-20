b32125d652e526ec3edc93c811d9859d405e18fd
lucas-perata <contacto.perata@gmail.com>
2023-03-20 17:31:22 -0300

    Daisy UI

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,5 +12,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require("daisyui")],
 };
