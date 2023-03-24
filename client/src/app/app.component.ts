3dbc883a0e31961e60534b1bd75da05a19c0518c
lucas-perata <contacto.perata@gmail.com>
2023-03-23 22:17:32 -0300

    Login and register client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +12,26 @@ export class AppComponent implements OnInit {
   title = 'DGaming App';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.http.get("https://localhost:7174/api/users").subscribe({
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers (){
+    this.http.get("https://localhost:5001/api/users").subscribe({
       next: response => this.users = response,
       error: error => {console.log(error)},
       complete: () => {console.log("Request (users) has completed")}
     });
+  }
+
+  setCurrentUser() {
+    const user : User = JSON.parse(localStorage.getItem('user') as string);
+    console.log(user);
+
+    this.accountService.setCurrentUser(user);
   }
   
 }
