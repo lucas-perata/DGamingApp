fc16a35957f401dabf5add057223d9e5b17949ee
lucas-perata <contacto.perata@gmail.com>
2023-03-30 16:55:56 -0300

    Fix error in the DB

--- DGamingApp/Dto/MemberDto.cs
+++ DGamingApp/Dto/MemberDto.cs
@@ -15,6 +15,7 @@ namespace DGamingApp.Dto
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string FavoriteGames { get; set; }
+        public string Interests { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
         public List<PhotoDto> Photos { get; set; } = new();
