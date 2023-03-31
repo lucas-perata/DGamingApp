d6c96508c6c857a1e8ff56bfec558980fd86dbab
lucas-perata <contacto.perata@gmail.com>
2023-03-31 09:47:30 -0300

    Fix token service

--- DGamingApp/Services/TokenService.cs
+++ DGamingApp/Services/TokenService.cs
@@ -14,20 +14,21 @@ namespace DGamingApp.Services
         {
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
         }
-        public string CreateToken(AppUser user)
+         string ITokenService.CreateToken(AppUser user)
         {
-            var claims = new List<Claim>();
+              var claims = new List<Claim>
             {
-                new Claim(JwtRegisteredClaimNames.NameId, user.UserName);
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
             };
 
-            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var cred = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.Now.AddDays(7),
-                SigningCredentials = creds,
+                SigningCredentials = cred,
             };
 
             var tokenHandler = new JwtSecurityTokenHandler();
