8b4d18e6fbce3e4671ac7236b93c6eac3e447ad3
lucas-perata <contacto.perata@gmail.com>
2023-04-10 12:58:17 -0300

    Photo-edit fix

--- client/src/app/members/photo-edit/photo-edit.component.ts
+++ client/src/app/members/photo-edit/photo-edit.component.ts
@@ -84,6 +84,10 @@ export class PhotoEditComponent {
       if(response) {
         const photo = JSON.parse(response);
         this.member?.photos.push(photo);
+        if (photo.isMain && this.user && this.member) {
+          this.user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user);
+        }
       }
     }
   }
