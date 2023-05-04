fc17ae1c9624dcb0631c7fef042dac00bfe55e70
lucas-perata <contacto.perata@gmail.com>
2023-05-03 21:17:20 -0300

    Filter age

--- DGamingApp/Helpers/UserParams.cs
+++ DGamingApp/Helpers/UserParams.cs
@@ -12,7 +12,8 @@ namespace DGamingApp.Helpers
         }
         
         public string? CurrentUsername {get; set;}
-
         public string? Gender { get; set; }
+        public int MinAge { get; set; } = 18;  
+        public int MaxAge { get; set; } = 100; 
     }
 }
