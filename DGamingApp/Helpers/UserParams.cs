c10aa9dd6b1167e0bc89cec1e308068545777211
lucas-perata <contacto.perata@gmail.com>
2023-05-03 21:01:16 -0300

    Filter users gender

--- DGamingApp/Helpers/UserParams.cs
+++ DGamingApp/Helpers/UserParams.cs
@@ -11,5 +11,8 @@ namespace DGamingApp.Helpers
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value; 
         }
         
+        public string? CurrentUsername {get; set;}
+
+        public string? Gender { get; set; }
     }
 }
