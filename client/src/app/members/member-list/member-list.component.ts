8893b25599855bc5dca6e7253e19667bd3c137e7
lucas-perata <contacto.perata@gmail.com>
2023-12-19 15:56:27 -0300

    FRONT/cache filter

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { PageEvent } from '@angular/material/paginator';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { take } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { Member } from 'src/app/_models/member';
@@ -9,18 +9,17 @@ import { UserParams } from 'src/app/_models/userParams';
 import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 
-
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
 })
+
 export class MemberListComponent {
   members: Member[] = []; 
   pageEvent: PageEvent | undefined;
   pagination: Pagination | undefined;
   userParams: UserParams | undefined; 
-  user: User | undefined; 
   pageIndex = 0; 
   hidePageSize = false;
   showPageSizeOptions = true;
@@ -30,15 +29,8 @@ export class MemberListComponent {
   orderByList = [{value: "lastActive", display: "Last active"}, {value: "created", display: "New members"}];
 
 
-  constructor(private memberService: MembersService, private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user => {
-        if (user){
-          this.userParams = new UserParams(user);
-          this.user = user; 
-        }
-      }
-    })
+  constructor(private memberService: MembersService) {
+    this.userParams = this.memberService.getUserParams();
   }
 
   ngOnInit(): void {
@@ -46,32 +38,31 @@ export class MemberListComponent {
   }
 
   loadMembers() {
-    if (!this.userParams) return; 
-    this.memberService.getMembers(this.userParams).subscribe({
-      next: response => {
-        if (response.result && response.pagination) {
-          this.members = response.result; 
-          this.pagination = response.pagination;
+    if (this.userParams) {
+      this.memberService.setUserParams(this.userParams);
+      this.memberService.getMembers(this.userParams).subscribe({
+        next: response => {
+          if (response.result && response.pagination) {
+            this.members = response.result; 
+            this.pagination = response.pagination;
+          }
         }
-      }
-    })
+      })
+    }  
   }
 
   resetFilters(){
-    if (this.user){
-      this.userParams = new UserParams(this.user); 
+      this.userParams = this.memberService.resetUserParams(); 
       this.loadMembers(); 
-    }
   }
   
   handlePageEvent(e: PageEvent) {
-    if (this.userParams && this.userParams?.pageNumber !== e.pageIndex)
+    if (this.userParams)
     {
-    this.pageEvent = e;
-    this.userParams.pageSize = e.pageSize;
-    this.pageIndex = 1;
-    this.userParams.pageNumber = e.pageIndex + 1;
-    this.loadMembers();
+      this.userParams.pageSize = e.pageSize;
+      this.pageIndex = 0;
+      this.userParams.pageNumber = e.pageIndex + 1;
+      this.loadMembers();
     }
   }
 
