c268168989699ea7337a4a2bb7b049e1a7b26d9e
lucas-perata <contacto.perata@gmail.com>
2024-01-17 13:20:57 -0300

    Messages scrolling and nav

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -26,7 +26,8 @@ export class NavComponent implements OnInit{
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: _ =>  this.router.navigateByUrl("/members")})
+      next: _ =>  {this.router.navigateByUrl("/members");
+      this.model = {}}})
   }
 
   logout(){
